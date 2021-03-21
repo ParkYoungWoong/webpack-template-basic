8bbb3f39d30f5939e01383dba6f57ca7633fe6c9
ParkYoungWoong <thesecon@gmail.com>
2021-03-21 17:14:55 +0900

    Create project

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: './js/main.js',
+  output: {
+    // path: path.resolve(__dirname, 'dist'),
+    // filename: 'main.js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  devServer: {
+    host: 'localhost',
+    hot: true
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: './index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'static' }
+      ]
+    })
+  ]
+}
