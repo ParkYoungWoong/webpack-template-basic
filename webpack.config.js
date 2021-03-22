e892f5038c789069e24422e5b3c7606742b612fb
ParkYoungWoong <thesecon@gmail.com>
2021-03-22 21:00:47 +0900

    Add comment for webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,27 @@
+// path: NodeJS에서 파일 및 디렉토리 경로 작업을 위한 전역 모듈
 const path = require('path')
 const HtmlPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
+  // 파일을 읽어들이기 시작하는 진입점 설정
   entry: './js/main.js',
+
+  // 결과물(번들)을 반환하는 설정
   output: {
-    // 기본값!
+    // 주석은 기본값!, `__dirname`은 현재 파일의 위치를 알려주는 NodeJS 전역 변수
     // path: path.resolve(__dirname, 'dist'),
     // filename: 'main.js',
     clean: true
   },
+  
+  // 모듈 처리 방식을 설정
   module: {
     rules: [
       {
         test: /\.s?css$/,
         use: [
+          // 순서 중요!
           'style-loader',
           'css-loader',
           'postcss-loader',
@@ -23,12 +30,15 @@ module.exports = {
       },
       {
         test: /\.js/,
+        exclude: /node_modules/, // 제외할 경로
         use: [
           'babel-loader'
         ]
       }
     ]
   },
+
+  // 번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
   plugins: [
     new HtmlPlugin({
       template: './index.html',
@@ -39,6 +49,8 @@ module.exports = {
       ]
     })
   ],
+
+  // 개발 서버 옵션
   devServer: {
     host: 'localhost',
     port: 8080,
